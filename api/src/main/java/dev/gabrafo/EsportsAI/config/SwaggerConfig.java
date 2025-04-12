2708fc88a0b319ff4a6e8ce6c664b1dfeb147a52
Gabrafo <gafagundesmesquita@gmail.com>
2025-04-12 09:57:24 -0300

    fix: correcting directories order

---
+++ api/src/main/java/dev/gabrafo/EsportsAI/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package dev.gabrafo.EsportsAI.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "API Esports AI", version = "v1"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
+        return new io.swagger.v3.oas.models.OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
+                .info(new Info()
+                        .title("Esports AI Backend")
+                        .version("1.0.0")
+                        .description("API for Esports AI system")
+                        .contact(new Contact()
+                                .name("Creator")
+                                .url("https://www.github.com/gabrafo")
+                                .url("https://www.linkedin.com/in/gafagundes")
+                        )
+                );
+    }
+}
